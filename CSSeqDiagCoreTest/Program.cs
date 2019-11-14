dc49b9c39f90877668dc8e3d9751917cc261dba3
d7p4n4 <bercikezs199999999@gmail.com>
2019-11-14 07:37:05 +0100

    Add project files.

---
+++ CSSeqDiagCoreTest/Program.cs
@@ -0,0 +1,45 @@
+﻿
+using CSAc4yObjectService.Class;
+using d7p4n4Namespace.EFMethods.Class;
+using d7p4n4Namespace.Final.Class;
+using d7p4n4Namespace.PersistentService.Class;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace CSAc4yEntityLibraryTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ac4ySDSequenceEntityMethods ac4YSDSequenceEntityMethods = new Ac4ySDSequenceEntityMethods();
+            Ac4ySDSequencePersistentService ac4YSDSequencePersistentService = new Ac4ySDSequencePersistentService();
+
+            Ac4ySDSequence sequence = new Ac4ySDSequence()
+            {
+                Communication = new List<Ac4ySDCommunication>()
+               {
+                   new Ac4ySDCommunication()
+                   {
+                       Sender = new Ac4ySDParticipant(){ Name = "frontend" }
+                       ,Receiver = new Ac4ySDParticipant(){ Name = "backend" }
+                       ,Message = new Ac4ySDMessage(){ Message = "login" }
+                   }
+                   ,new Ac4ySDCommunication()
+                   {
+                       Sender = new Ac4ySDParticipant(){ Name = "backend" }
+                       ,Receiver = new Ac4ySDParticipant(){ Name = "frontend" }
+                       ,Message = new Ac4ySDMessage(){ Message = "ok" }
+                   }
+               }
+            };
+
+            ac4YSDSequenceEntityMethods.SaveWithXml(sequence);
+        }
+    }
+}
